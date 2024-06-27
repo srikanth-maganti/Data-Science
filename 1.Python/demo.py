e437ddee374f6cc645059db9ebc3576b6ed9b4ce
srikanth-maganti <magantisrikanth45@gmail.com>
2024-06-27 14:10:18 +0530

    new filess

--- 1.Python/demo.py
+++ 1.Python/demo.py
@@ -1,15 +1,15 @@
 
-# import name
-# print("in demo",__name__)
+import name
+print("in demo",__name__)
 
 
-def add():
-    print("adeed")
-def sub():
-    print("subb")
-def main():
-    add()
-    sub()
+# def add():
+#     print("adeed")
+# def sub():
+#     print("subb")
+# def main():
+#     add()
+#     sub()
 
-if __name__=="__main__":#i want to exceute entire file only if it is excecuted as standalone file otherwise wont
-        main()
+# if __name__=="__main__":#i want to exceute entire file only if it is excecuted as standalone file otherwise wont
+#         main()
--- 1.Python/name.py
+++ 1.Python/name.py
@@ -1,23 +1,24 @@
 #If the source file is executed as the main program, the interpreter sets the __name__ variable to have a value “__main__”. If this file is being imported from another module, __name__ will be set to the module’s name. __name__ is a built-in variable which evaluates to the name of the current module
-# def main():
-#     print("file excected as main program")
-# if __name__=="__main__":
-#     main()
-# else:
-#     print("file is being imported")
-
-
-
-from demo import add#the movement whenever i import demo file will be exccuted but i dont want that i just want add only
-def fun1():
-    add()
-    print("function1")
-
-def fun2():
-    print("function2")
-
-#in a standard way of programming everything in functions
 def main():
-    fun1()
-    fun2()
-main()
+    print("file excected as main program")
+if __name__=="__main__":
+    main()
+else:
+    print("file is being imported")
+    print(__name__)
+
+
+
+# from demo import add#the movement whenever i import demo file will be exccuted but i dont want that i just want add only
+# def fun1():
+#     add()
+#     print("function1")
+
+# def fun2():
+#     print("function2")
+
+# #in a standard way of programming everything in functions
+# def main():
+#     fun1()
+#     fun2()
+# main()
